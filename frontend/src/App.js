1bd001c4ad83e8abfc5fc8245ee0cf27afcb9f32
Ranjan-coder <ranjanchanda06@gmail.com>
2024-05-23 19:19:40 +0530

    folder stracture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Dashboard from './Pages/Patient/Dashboard/Dashboard';
 
 function App() {
   return (
     <>
-    <div>medi ai</div>
+    <Dashboard/>
     </>
   );
 }
---
+++ frontend/src/Pages/Patient/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DashboardStyle from './Dashboard.module.css'
+
+const Dashboard = () => {
+  return (
+    <>
+    <div className={DashboardStyle.backgroundcolour}>
+        <div></div>
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
